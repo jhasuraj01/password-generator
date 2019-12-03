b4a311b38a7203623e235c85c7ed86aca7b5bb29
the-suraj <surajsjjha@gmail.com>
2019-12-03 23:52:29 +0530

    Stick header ar the top if screen height is more than 35em

--- scripts/style.js
+++ scripts/style.js
@@ -1,6 +1,9 @@
 // rotate the refresh button on click;
 
 let refresh_password_btn = document.getElementById('refresh-password-btn');
+let password_container = document.getElementsByClassName('password-container')[0];
+let body_header = document.getElementById('body-header');
+
 refresh_password_btn.addEventListener('click', () => {
     if (refresh_password_btn.classList.contains('rotate')) {
         refresh_password_btn.classList.remove('rotate');
@@ -12,3 +15,16 @@ refresh_password_btn.addEventListener('click', () => {
         }
     }, 500);
 });
+
+let media = window.matchMedia('(min-height: 35em)');
+
+let handleMedia = (media) => {
+    if (media.matches) {
+        console.log(body_header);
+        password_container.style.top = `${body_header.clientHeight}px`;
+    } else {
+        password_container.style.top = "0";
+    }
+}
+handleMedia(media); // Call listener function at run time
+media.addListener(handleMedia); // Attach listener function on state changes 
