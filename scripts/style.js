9aa1665e4613b8e76eacc993220ed7be5e5dd76f
the-suraj <surajsjjha@gmail.com>
2019-12-05 02:37:08 +0530

    Nav Bar Added

--- scripts/style.js
+++ scripts/style.js
@@ -3,6 +3,10 @@
 let refresh_password_btn = document.getElementById('refresh-password-btn');
 let password_container = document.getElementsByClassName('password-container')[0];
 let body_header = document.getElementById('body-header');
+let main = document.getElementById('main');
+let body_footer = document.getElementById('body-footer');
+let sideNavBar = document.getElementById('side-navBar');
+let sidebar_btn_checkBox = document.getElementById('sidebar-btn-checkBox');
 
 refresh_password_btn.addEventListener('click', () => {
     if (refresh_password_btn.classList.contains('rotate')) {
@@ -16,15 +20,57 @@ refresh_password_btn.addEventListener('click', () => {
     }, 500);
 });
 
-let media = window.matchMedia('(min-height: 37em)');
 
-let handleMedia = (media) => {
-    if (media.matches) {
-        console.log(body_header);
-        password_container.style.top = `${body_header.clientHeight}px`;
+let media_forHeader = window.matchMedia('(min-height: 37em)');
+
+let repositionSideNavBar = () => {
+    if (sidebar_btn_checkBox.checked === true) {
+        sideNavBar.style.transform = "translateX(0)";
+        if (!media_forHeader.matches) {
+            body_header.style.transform = `translateX(${sideNavBar.offsetWidth}px)`;
+            body_footer.style.transform = `translateX(${sideNavBar.offsetWidth}px)`;
+            main.style.transform = `translateX(${sideNavBar.offsetWidth}px)`;
+        } else {
+            body_header.style.transform = "translateX(0)";
+            main.style.transform = "translateX(0)";
+            body_footer.style.transform = "translateX(0)";
+        }
     } else {
-        password_container.style.top = "0";
+        sideNavBar.style.transform = "translateX(calc(-100% - 6px))";
+        body_header.style.transform = "translateX(0)";
+        main.style.transform = "translateX(0)";
+        body_footer.style.transform = "translateX(0)";
     }
 }
-handleMedia(media); // Call listener function at run time
-media.addListener(handleMedia); // Attach listener function on state changes 
+
+let handle_Media_forHeader = (media_forHeader) => {
+    if (media_forHeader.matches) {
+        password_container.style.top = `${body_header.clientHeight}px`;
+        sideNavBar.style.setProperty('--top', `${body_header.clientHeight}px`);
+        sideNavBar.style.zIndex = '2';
+    } else {
+        password_container.style.top = "0";
+        sideNavBar.style.setProperty('--top', "0px");
+        sideNavBar.style.zIndex = '4';
+    }
+    repositionSideNavBar();
+}
+
+handle_Media_forHeader(media_forHeader); // Call listener function at run time
+media_forHeader.addListener(handle_Media_forHeader); // Attach listener function on state changes 
+
+
+body_header.style.transition = "transform 300ms cubic-bezier(0.4, 0.0, 0.2, 1)";
+body_footer.style.transition = "transform 300ms cubic-bezier(0.4, 0.0, 0.2, 1)";
+main.style.transition = "transform 300ms cubic-bezier(0.4, 0.0, 0.2, 1)";
+sideNavBar.style.transition = "transform 300ms cubic-bezier(0.4, 0.0, 0.2, 1)";
+
+
+sidebar_btn_checkBox.addEventListener('change', repositionSideNavBar);
+window.addEventListener('resize', () => {
+    repositionSideNavBar();
+});
+window.addEventListener('orientationchange', () => {
+    repositionSideNavBar();
+});
+repositionSideNavBar();
