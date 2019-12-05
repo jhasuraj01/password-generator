32cbbe9734701e176d5b2fc5a4c1ca81c5ce58b2
the-suraj <surajsjjha@gmail.com>
2019-12-05 19:36:31 +0530

    service worker location corrected

--- registerSW.js
+++ registerSW.js
@@ -2,7 +2,7 @@ let registerSW = () => {
     // first check if service worker is supported in the browser
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('../SW.js')
+            .register('./SW.js')
             .then(reg => console.log('Service Worker Registered'))
             .catch(err => console.log('Service Worker Registeration error: ' + err));
     }
