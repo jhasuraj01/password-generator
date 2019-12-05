5bb47c9895e36896e9e010b44ecb4b34e55a69c7
the-suraj <surajsjjha@gmail.com>
2019-12-05 16:35:09 +0530

    implemented share feature for whatsapp and twitter

--- scripts/share.js
+++ scripts/share.js
@@ -1,12 +1,30 @@
 let popitup = (rqr_win = {name: 'new Window', url: 'https://example.com'}) => {
     console.log(rqr_win);
-    newwindow = window.open(rqr_win.url, rqr_win.name, `height=500,width=300`);
+    newwindow = window.open(rqr_win.url, rqr_win.name, "height=500,width=700");
     newwindow.focus();
 };
 
-document.getElementById('fb-share').addEventListener('click', () => {
+document.getElementById('whatsapp-share').addEventListener('click', () => {
+    popitup({
+        name: 'share on WhatsApp',
+        url: 'https://api.whatsapp.com/send?phone&text='+ encodeURIComponent(window.location.href)
+    })
+});
+document.getElementById('facebook-share').addEventListener('click', () => {
     popitup({
         name: 'share on facebook',
         url: 'https://www.facebook.com/sharer/sharer.php?&u='+ encodeURIComponent(window.location.href)
     })
-})
+});
+// document.getElementById('instagram-share').addEventListener('click', () => {
+//     popitup({
+//         name: 'share on Instagram',
+//         url: 'https://www.facebook.com/sharer/sharer.php?&u='+ encodeURIComponent(window.location.href)
+//     })
+// })
+document.getElementById('twitter-share').addEventListener('click', () => {
+    popitup({
+        name: 'share on Twitter',
+        url: 'https://twitter.com/intent/tweet?text='+ encodeURIComponent(window.location.href)
+    })
+});
